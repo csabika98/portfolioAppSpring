56853260172b323b174175ff9803973b677ffd7b
csabika98 <sallcsa.csaba8@gmail.com>
2024-06-11 15:37:38 +0200

    NOTICKET Initial Commit

---
+++ src/main/kotlin/com/csabika98/PortfolioAppApplication.kt
@@ -0,0 +1,11 @@
+package com.csabika98
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/csabika98/PortfolioAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.csabika98
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
